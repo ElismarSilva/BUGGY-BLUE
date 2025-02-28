046fad5889e3b472d4ae4fb4a353ca6b1cf468cc
Elismar_Dev <100629483+ElismarSilva@users.noreply.github.com>
2025-02-28 16:55:35 -0300

    Update 🚀buggy.ino

--- 🚀buggy.ino
+++ 🚀buggy.ino
@@ -1,21 +1,21 @@
-
-/*Projeto buggy desenvolvido por Elismar Silva
-  19 de Novembro 2022
-   Github(https://github.com/Olamundoio)
+/* Projeto buggy desenvolvido por Elismar Silva
+   19 de Novembro 2022
+   Github(https://github.com/ElismarSilva)
 */
 
-//inclusao das blibliotecas>>
-#include <SensorUltrasonico.h> //inclusao da biblioteca 
+// Inclusão das bibliotecas
+#include <SensorUltrasonico.h>  // Inclusão da biblioteca do sensor ultrassônico
 #include <Servo.h>
-#define direcao 2  // pino do servo da direcao
-#define motor_E 3 // pino do servo do lado esquerdo
-#define motor_D 4 // pino do servo do lado direito
-#define farol   5 // pino dos farois 
-#define buzina  7 // pino da buzina 
+
+#define direcao 2  // Pino do servo da direção
+#define motor_E 3  // Pino do servo do lado esquerdo
+#define motor_D 4  // Pino do servo do lado direito
+#define farol 5    // Pino dos faróis
+#define buzina 7   // Pino da buzina
 
 SensorUltrasonico dist_Cm(10, 11);
 
-// criacao de objeto dos servos
+// Criação dos objetos dos servos
 Servo motor_Esquerdo;
 Servo motor_Direito;
 Servo motor_direcao;
@@ -27,107 +27,117 @@ void direcao_D();
 void Frente();
 void Tras();
 
-
+bool aux = false;  // Variável para alternar entre andar e parar
 
 void setup() {
-
   motor_direcao.attach(direcao);
-  motor_Esquerdo.attach( motor_E);
+  motor_Esquerdo.attach(motor_E);
   motor_Direito.attach(motor_D);
   pinMode(farol, OUTPUT);
   pinMode(buzina, OUTPUT);
   Serial.begin(9600);
   digitalWrite(farol, HIGH);
   direcao_Centro();
-
 }
 
 void loop() {
   double dist = dist_Cm.distanciaCM();
   Serial.println(dist);
-  //Variaveis auxiliares
-  static boolean oN_Off = false;  // variavel aux do comando farol('L' liga / desliga)
-  static boolean aux = false; // variavel aux do comando frente('F' frente / tras)
 
-
-  if(Serial.available()) {
+  if (Serial.available()) {
     char comando = Serial.read();
     Serial.println(comando);
 
-
     switch (comando) {
+      case 'F':
+        {
+          if (dist > 15) {
+            aux = !aux;  // Alterna entre andar e parar
+          }
 
-      case 'F': {
-          aux = !aux;
-          if (aux == true) {
+          if (aux == true && dist > 15) {
             Frente();
-          } else
+          } else {
             Parado();
+          }
           break;
         }
-
-      case 'T': {
+      case 'T':
+        {
           Tras();
           break;
         }
-      case't': {
+      case 'P':
+        {
           Parado();
           break;
         }
-
-      case 'E': {
+      case 'E':
+        {
           direcao_E();
           break;
         }
-      case 'D': {
+      case 'D':
+        {
           direcao_D();
           break;
         }
-      case 'C': {
+      case 'C':
+        {
           direcao_Centro();
           break;
         }
-      case 'B': {
+      case 'B':
+        {
           for (int i = 0; i < 2; i++) {
             tone(buzina, 280);
             delay(100);
             noTone(buzina);
             delay(100);
           }
+          break;
+        }
+      case 'L':
+        {
+          static bool oN_Off = false;
+          oN_Off = !oN_Off;
+          digitalWrite(farol, oN_Off);
+          break;
         }
     }
+  }
 
-    if (comando == 'L' ) {
-      oN_Off = !oN_Off;
-      if (oN_Off == true)
-        digitalWrite(farol, true);
-      else
-        digitalWrite(farol, false);
-    }
-
+  // Se o robô estiver andando para frente e um obstáculo aparecer, ele para
+  if (aux && dist <= 15) {
+    Parado();
+    aux = false;
   }
 }
 
-//Funções
+// Funções de controle do robô
 void Frente() {
-  motor_Esquerdo.write(180); // os motores se movimentam no sentido horario
+  motor_Esquerdo.write(180);
   motor_Direito.write(0);
 }
+
 void Tras() {
-  motor_Esquerdo.write(0); // os mtores se movimentam no sentido anti-horario
+  motor_Esquerdo.write(0);
   motor_Direito.write(180);
 }
+
 void Parado() {
-  motor_Esquerdo.write(90); // os motores param de girar
+  motor_Esquerdo.write(90);
   motor_Direito.write(90);
 }
-void direcao_E() {
-  motor_direcao.write(120); // o servo motor gira para esquerda
 
+void direcao_E() {
+  motor_direcao.write(120);
 }
+
 void direcao_Centro() {
-  motor_direcao.write(97); // o servo motor fica centralizado
+  motor_direcao.write(97);
 }
+
 void direcao_D() {
-  motor_direcao.write(74); // o servo motor gira para a direita
+  motor_direcao.write(74);
 }
